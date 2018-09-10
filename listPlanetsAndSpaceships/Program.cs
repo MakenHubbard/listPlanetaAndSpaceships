ab2da5ff1d1c053cf5a9e8adff8dcce8d3ddfa44
Maken Hubbard <makhub26@outlook.com>
2018-09-10 16:08:10 -0500

    adding the funtinality to the planets list

--- listPlanetsAndSpaceships/Program.cs
+++ listPlanetsAndSpaceships/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace listPlanetsAndSpaceships
 {
@@ -6,7 +7,29 @@ namespace listPlanetsAndSpaceships
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a list of planets
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            // Add Jupiter and Saturn to the list
+            planetList.AddRange(new string[] { "Jupiter", "Saturn" });
+
+            // Create another list and add the last two of our planets to this list
+
+            List<string> lastTwoPlanets = new List<string>() { "Neptune", "Pluto" };
+
+            // Comnbine the two list using addRange()
+            planetList.AddRange(lastTwoPlanets);
+
+            // Use Insert() to add Earth and Venus to the list in the correct order
+            planetList.InsertRange(1, new List<string>() { "Venus", "Earth" });
+            
+
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+;
+            Console.ReadLine();
         }
     }
 }
