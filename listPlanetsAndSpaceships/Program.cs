027cca3cc201400e29c4a528643bcf5ccbd7022f
Maken Hubbard <makhub26@outlook.com>
2018-09-10 13:36:20 -0500

    Add project files.

---
+++ listPlanetsAndSpaceships/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace listPlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
