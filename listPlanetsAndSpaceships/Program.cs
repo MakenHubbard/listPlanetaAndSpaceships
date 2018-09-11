41ca1fda380db3df01eb12eddf5e1b315ea4ba3e
Maken Hubbard <makhub26@outlook.com>
2018-09-10 21:21:40 -0500

    adding code for the finished planets

--- listPlanetsAndSpaceships/Program.cs
+++ listPlanetsAndSpaceships/Program.cs
@@ -19,16 +19,40 @@ namespace listPlanetsAndSpaceships
 
             // Comnbine the two list using addRange()
             planetList.AddRange(lastTwoPlanets);
+            planetList.Remove("Pluto");
 
             // Use Insert() to add Earth and Venus to the list in the correct order
             planetList.InsertRange(1, new List<string>() { "Venus", "Earth" });
-            
 
+            // Use add() to add Pluto to the list
+            planetList.Add("Pluto");
+
+            planetList.Remove("Pluto");
+
+
+
+            // Looping over the all the planets list
             foreach (var planet in planetList)
             {
                 Console.WriteLine(planet);
             }
-;
+
+            Console.Write("Here are the planets that have a rocky surface: ");
+
+            // use GetRange() in order to extract the rocky planets into another list
+            var rockyPlanets = planetList.GetRange(0, 4);
+           
+
+
+            // Looping over the rocky planets list
+            foreach (var rockies in rockyPlanets)
+            {
+                rockies.Split(" ");
+                Console.WriteLine(rockies);
+            }
+
+
+
             Console.ReadLine();
         }
     }
